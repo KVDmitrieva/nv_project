574c309430b1122fc1a0803b1e8f634088c07ec1
kvdmitrieva <katedv02@gmail.com>
2023-11-29 21:11:11 +0300

    cut audio

--- hw_nv/collate_fn/collate.py
+++ hw_nv/collate_fn/collate.py
@@ -10,17 +10,12 @@ def collate_fn(dataset_items: List[dict]):
     Collate and pad fields in dataset items
     """
     spectrogram, audio = [], []
-    spectrogram_length, audio_path = [], []
 
     for item in dataset_items:
         audio.append(item["audio"])
-        audio_path.append(item["audio_path"])
         spectrogram.append(item["spectrogram"].squeeze(0).T)
-        spectrogram_length.append(item["spectrogram"].shape[2])
 
     return {
         "audio": audio,
-        "audio_path": audio_path,
-        "mel_length": torch.tensor(spectrogram_length),
         "mel":  torch.nn.utils.rnn.pad_sequence(spectrogram, batch_first=True).transpose(1, 2)
     }
