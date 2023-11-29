4a01c127f680ee79b161033490a9cdb569eddccf
kvdmitrieva <katedv02@gmail.com>
2023-11-29 15:34:53 +0300

    fix losses

--- hw_nv/loss/DiscriminatorLoss.py
+++ hw_nv/loss/DiscriminatorLoss.py
@@ -5,16 +5,12 @@ import torch.nn as nn
 class DiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.mse_loss = nn.MSELoss()
-        self.l1_loss = nn.L1Loss()
 
     def forward(self, gen_discriminator_out, real_discriminator_out, **batch):
         adv_loss = 0.0
-        for d_out in gen_discriminator_out:
-            adv_loss += self.mse_loss(d_out, torch.zeros_like(d_out, device=gen_discriminator_out.device))
 
-        for d_out in real_discriminator_out:
-            adv_loss += self.mse_loss(d_out, torch.ones_like(d_out, device=gen_discriminator_out.device))
+        for real, gen in zip(real_discriminator_out, gen_discriminator_out):
+            adv_loss += torch.mean((1 - real) ** 2) + torch.mean(gen ** 2)
 
         return {
             "discriminator_loss": adv_loss,
--- hw_nv/loss/GeneratorLoss.py
+++ hw_nv/loss/GeneratorLoss.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 class GeneratorLoss(nn.Module):
     def __init__(self, mel_lambda, fm_lambda):
         super().__init__()
-        self.mse_loss = nn.MSELoss()
         self.l1_loss = nn.L1Loss()
         self.mel_lambda = mel_lambda
         self.fm_lambda = fm_lambda
@@ -13,16 +12,14 @@ class GeneratorLoss(nn.Module):
     def forward(self, gen_discriminator_out, mel, gen_mel, real_feature_map, gen_feature_map, **batch):
         adv_loss = 0.0
         for d_out in gen_discriminator_out:
-            adv_loss += self.mse_loss(d_out, torch.ones_like(d_out, device=gen_discriminator_out.device))
+            adv_loss += torch.mean((1 - d_out) ** 2)
 
         mel_loss = self.l1_loss(mel, gen_mel)
-        feature_loss = 0.0
 
+        feature_loss = 0.0
         for real, gen in zip(real_feature_map, gen_feature_map):
-            tmp = 0.0
-            for i in range(len(real)):
-                tmp += self.l1_loss(real[i], gen[i])
-            feature_loss += tmp / len(real)
+            for r, g in zip(real, gen):
+                feature_loss += self.l1_loss(r, g)
 
         return {
             "gen_loss": adv_loss + self.mel_lambda * mel_loss + self.fm_lambda * feature_loss,
