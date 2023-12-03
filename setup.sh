2b0da8008db3dca8a102fdb01c0142ae51237783
kvdmitrieva <katedv02@gmail.com>
2023-12-03 14:25:30 +0300

    final touch

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 
 echo "Download checkpoint"
 gdown "https://drive.google.com/u/0/uc?id=1o_4tceuFzwYgexKVwEDIXhLKxWlwFKj_" -O default_test_model/config.json
-gdown "https://drive.google.com/u/0/uc?id=1HEYwStqS2hTPZljwWGHi7et7pkUhYgHz" -O default_test_model/model.pth
+gdown "https://drive.google.com/u/0/uc?id=1zFmPTrP4sG2ZulWRGyoQwjFZTgpN5ff_" -O default_test_model/model.pth
